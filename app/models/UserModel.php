e4a2472114c22f5e38716fbc9ec1014600fa9d7c
quocha <quochait1610@gmail.com>
2020-05-02 14:19:55 +0700

    Add controller Login Logout

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -5,33 +5,34 @@
 		//Kiểm tra xem user đó có tồn tại trong csdl hay chưa
 		public function checkExistedUser($email){
 			$qr = "SELECT * FROM users where email = '$email' limit 1";
-            if(mysqli_num_rows(mysqli_query($this->con, $qr)) > 0){
-                return true;
-            }
-            return false;
+      if(mysqli_num_rows(mysqli_query($this->con, $qr)) > 0){
+          return true;
+      }
+
+      return false;
 		}
 
-
 		//Kiểm tra user và password có đúng ko
-		public function checkUserAndPasswordIsCorrect($username, $password){
-            $qr = "SELECT * FROM users where username = '$username' and password = '$password' limit 1";
-        	return mysqli_fetch_array(mysqli_query($this->con, $qr));
-        }
+		public function checkUserAndPasswordIsCorrect($email, $password){
+			$password = md5($password);
+			$qr = "SELECT username, fullname, role_id FROM users where email = '$email' and password = '$password' limit 1";
+			$result = mysqli_query($this->con, $qr);
+    	return $result;
+    }
 
+    //Thêm user mới với quyền mặc định là 1
+    public function addUser($username, $password, $email, $fullname, $role_id = 1){
 
-        //Thêm user mới với quyền mặc định là 1
-        public function addUser($username, $password, $email, $fullname, $role_id = 1){
+    	$password = md5($password);
 
-        	$password = md5($password);
+    	$qr = "INSERT INTO users(username, password, email, fullname, role_id) values ('$username', '$password', '$email', '$fullname', '$role_id')";
 
-        	$qr = "INSERT INTO users(username, password, email, fullname, role_id) values ('$username', '$password', '$email', '$fullname', '$role_id')";
+    	if(mysqli_query($this->con, $qr)){
+    		return true;
+    	}
 
-        	if(mysqli_query($this->con, $qr)){
-        		return true;
-        	}
-
-        	return false;
-        }
+    	return false;
+    }
 	}
 
 
