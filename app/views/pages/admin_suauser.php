dd617d5e6ad0a02980adc0a684632aff294164e2
thanhdinhiuh <thanhdinhiuh@gmail.com>
2020-05-16 16:24:29 +0700

    sua type input

--- app/views/pages/admin_suauser.php
+++ app/views/pages/admin_suauser.php
@@ -23,38 +23,38 @@
   <!-- Horizontal Form -->
   <div class="box box-info">
             <div class="box-header with-border">
-           
+              <h3 class="box-title">Horizontal Form</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
             <form class="form-horizontal">
               <div class="box-body">
                 <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                  <label for="email" class="col-sm-2 control-label">Email</label>
 
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    <input type="email" class="form-control" id="email" placeholder="Email">
                   </div>
                 </div>
                 <div class="form-group">
                   <label for="UserName" class="col-sm-2 control-label">UserName</label>
 
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="UserName" placeholder="UserName">
+                    <input type="text" class="form-control" id="UserName" placeholder="UserName">
                   </div>
                 </div>
                 <div class="form-group">
                   <label for="FullName" class="col-sm-2 control-label">FullName</label>
 
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="FullName" placeholder="FullName">
+                    <input type="text" class="form-control" id="FullName" placeholder="FullName">
                   </div>
                 </div>
                 <div class="form-group">
                   <label for="RoleID" class="col-sm-2 control-label">RoleID</label>
 
                   <div class="col-sm-10">
-                    <input type="email" class="form-control" id="RoleID" placeholder="RoleID">
+                    <input type="text" class="form-control" id="RoleID" placeholder="RoleID">
                   </div>
                 </div>
                 <div class="form-group">
