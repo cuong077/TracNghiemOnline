b8f4698bd58a2e88c313df69e39d9c20d1506fc6
cuong077 <57596058+cuong077@users.noreply.github.com>
2020-05-16 17:37:32 +0700

    Revert "Add controller admin user"

--- app/models/GradesModel.php
+++ app/models/GradesModel.php
@@ -2,37 +2,6 @@
 
 	class GradesModel extends DB{
 
-		//lay danh sach khoi lop hoc
-		public function getGrades()
-		{
-			$qr = "SELECT id, name FROM grades";
-			$result = mysqli_query($this->con, $qr);
-
-			return $result;
-		}
-
-		//Kiem tra mon hoc ton tai voi ten
-		public function existGradeWithName($gradeName){
-			$qr = "SELECT * FROM grades WHERE name='$gradeName' LIMIT 1";
-
-			if(mysqli_num_rows(mysqli_query($this->con, $qr)) > 0){
-				return true;
-			}
-
-			return false;
-		}
-
-		//Kiem tra mon hoc ton tai voi id
-		public function existGradeWithId($grandId){
-			$qr = "SELECT * FROM grades WHERE id='$grandId' LIMIT 1";
-
-			if(mysqli_num_rows(mysqli_query($this->con, $qr)) > 0){
-				return true;
-			}
-
-			return false;
-		}
-
     //Them khoi hoc
     public function addGrade($gradeName){
     	$qr = "INSERT INTO grades(name) VALUES ('$gradeName')";
